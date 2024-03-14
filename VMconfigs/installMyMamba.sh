5b8f198bde561a3a8b3fdcada47b4fd10307a0de
pescini <47242185+pescini@users.noreply.github.com>
2024-03-14 17:54:41 +0100

    Added the automatic activation of the wh env in bashrc

--- VMconfigs/installMyMamba.sh
+++ VMconfigs/installMyMamba.sh
@@ -13,6 +13,8 @@ mkdir -p ~/bin
 echo "# 1. Installing mamba"
 echo "#"
 
+MAMBAROOT=~/miniforge3
+
 # Installing mamba
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh";
 bash Miniforge3-$(uname)-$(uname -m).sh -b;
@@ -23,4 +25,14 @@ mamba init;
 source "${HOME}/.bashrc";
 mamba activate;
 wget https://raw.githubusercontent.com/qLSLab/CBCourse/main/VMconfigs/mambaEnvWH.yml;
-mamba env update wh --file mambaEnvWH.yml;
+mamba env update -n wh --file mambaEnvWH.yml;
+
+if [ -d "$MAMBAROOT/envs/wh" ]; then
+  echo "write in bashrc"
+  echo "" >> ~/.bashrc
+  echo "mamba activate wh" >> ~/.bashrc
+  echo "" >> ~/.bashrc
+fi
+
+source "${HOME}/.bashrc";
+
