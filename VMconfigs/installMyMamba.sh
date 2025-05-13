9ddae975a62359a310a4f6cbe1a7b8cf38ab89a3
Dario Pescini <dario.pescini@unimib.it>
2025-05-13 08:56:52 +0200

    new version of the installer

--- VMconfigs/installMyMamba.sh
+++ VMconfigs/installMyMamba.sh
@@ -18,14 +18,15 @@ MAMBAROOT=~/miniforge3
 # Installing mamba
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh";
 bash Miniforge3-$(uname)-$(uname -m).sh -b;
-source source "${HOME}/miniforge/etc/profile.d/conda.sh"
+source "${MAMBAROOT}/etc/profile.d/conda.sh"
 # For mamba support also run the following command
-source "${HOME}/miniforge/etc/profile.d/mamba.sh"
-~/miniforge3/bin/mamba init;
-source "${HOME}/.bashrc";
-~/miniforge3/mamba activate;
+mamba shell init;
+# source "${HOME}/.bashrc";
+eval "$(mamba shell hook --shell bash)";
+mamba activate;
 wget https://raw.githubusercontent.com/qLSLab/CBCourse/main/VMconfigs/mambaEnvWH.yml;
-~/miniforge3/mamba env update -n wh --file mambaEnvWH.yml;
+mamba create -n wh;
+mamba env update -n wh --file mambaEnvWH.yml;
 
 if [ -d "$MAMBAROOT/envs/wh" ]; then
   echo "write in bashrc"
